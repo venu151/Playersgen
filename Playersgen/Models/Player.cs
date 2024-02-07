f2d42fe1719be301a65c779fcde9bfa28069576d
vadde <vadde@LAPTOP-SBAQO473>
2024-02-07 14:47:51 +0530

    Add project files.

---
+++ Playersgen/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Playersgen.Models
+{
+    public partial class Player
+    {
+        public int PlayerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? JerseyNumber { get; set; }
+        public string? Position { get; set; }
+        public string? Team { get; set; }
+    }
+}
